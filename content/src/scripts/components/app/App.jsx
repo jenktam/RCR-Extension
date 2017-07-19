62ad75f78cd2da6bf78f47051487b80442bc5788
Jen Tam <jennifer.k.tam@gmail.com>
2017-07-19 16:57:18 -0700

    first commit

---
+++ content/src/scripts/components/app/App.jsx
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0
+    };
+  }
+
+  componentDidMount() {
+    document.addEventListener('click', () => {
+      this.props.dispatch({
+        type: 'ADD_COUNT'
+      })
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        Count: {this.props.count}
+        <br />
+        <button>Copy me</button>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  count: state.count
+});
+
+
+export default connect(mapStateToProps)(App);
